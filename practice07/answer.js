01eaf57d08038a233f5a57cc79a5cb27e1a77b3b
Mark Jurkiewicz <markajurkiewicz@gmail.com>
2016-04-05 17:53:30 -0700

    completed number 20

--- practice07/answer.js
+++
@@ -1,4 +0,0 @@
-var pig = "Hello my name is Mark";
-function pigLatin(pig){
-    var
-}
--- practice20/answer.js
+++ practice20/answer.js
@@ -1,5 +1,29 @@
-function randomize_list(){
-    var input_array=['silver', 'lantern', 'wigglers', 'larry bird', 'SHAQ'];
+
+    var input_array=['silver', 'lantern', 'wigglers', 'larry bird', 'shaq', 'bud ice', 'boomshakalaka'];
     var output_array=[];
 
+    /**
+     * Shuffles an array randomly
+     * @param {Array} arr
+     * @return Array
+     */
+function shuffle(array) {
+    var m = array.length, t, i;
+
+    // While there remain elements to shuffle…
+    while (m) {
+
+        // Pick a remaining element…
+        i = Math.floor(Math.random() * m--);
+
+        // And swap it with the current element.
+        t = array[m];
+        array[m] = array[i];
+        array[i] = t;
+        output_array.push();
+    }
+
+    return array;
 }
+
+    console.log(shuffle(input_array));
